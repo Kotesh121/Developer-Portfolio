b2b43c4164bbeb6f91f40ad1cddb0444e567252a
Shubham Jani <shubhamjani1731@gmail.com>
2026-09-07 14:45:47 +0530

    Refactor PublicApp to use React Router for navigation

--- Portfolio/src/PublicApp.jsx
+++ Portfolio/src/PublicApp.jsx
@@ -1,20 +1,39 @@
+import { Routes, Route } from "react-router-dom";
 import IntroAnimation from "./Components/IntroAnimation";
 import BackgroundFx from "./Components/ParticlesBackground";
 import useLiteMode from "./hooks/useLiteMode";
 import Navbar from "./Components/Navbar";
 import SocialSidebar from "./Components/SocialSidebar";
 import BackToTop from "./Components/BackToTop";
+import ScrollToTop from "./Components/ScrollToTop";
 import Hero from "./Sections/Hero";
 import About from "./Sections/About";
 import Skills from "./Sections/Skills";
 import Projects from "./Sections/Projects";
 import Experience from "./Sections/Experience";
-import ResumeCallout from "./Sections/ResumeCallout";
+import Certifications from "./Sections/Certifications";
 import Contact from "./Sections/Contact";
 import Blogs from "./Sections/Blogs";
 import Footer from "./Components/Footer";
+import AllProjectsPage from "./pages/AllProjectsPage";
+import AllCertificationsPage from "./pages/AllCertificationsPage";
 import { usePortfolio } from "./context/PortfolioContext";
 import useRecordVisit from "./hooks/useRecordVisit";
+
+function HomeSections({ sections }) {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects featuredOnly={true} />
+      <Experience />
+      {sections?.certifications && <Certifications featuredOnly={true} />}
+      {sections?.blogs && <Blogs />}
+      <Contact />
+    </>
+  );
+}
 
 export default function PublicApp() {
   useLiteMode();
@@ -24,19 +43,17 @@ export default function PublicApp() {
 
   return (
     <>
+      <ScrollToTop />
       <IntroAnimation />
       <BackgroundFx />
       <Navbar />
       <SocialSidebar />
       <main className="relative w-full overflow-x-clip xl:pl-16">
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Experience />
-        <ResumeCallout />
-        {sections?.blogs && <Blogs />}
-        <Contact />
+        <Routes>
+          <Route path="/" element={<HomeSections sections={sections} />} />
+          <Route path="/projects" element={<AllProjectsPage />} />
+          <Route path="/certifications" element={<AllCertificationsPage />} />
+        </Routes>
       </main>
       <Footer />
       <BackToTop />
